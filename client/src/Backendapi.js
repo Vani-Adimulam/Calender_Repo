7bba389ed83227587bf8304cc95d0e02ce1c76e2
LAPTOP-4VM21UJB\vania <testrepositorybitbucket2@gmail.coom>
2024-05-10 12:25:54 +0530

    New ConferenceRoom Pro

--- client/src/Backendapi.js
+++ client/src/Backendapi.js
@@ -1,10 +1,7 @@
-
 const Backendapi = {
-    REACT_APP_BACKEND_API_URL:"http://172.16.90.3:9011",
-    REACT_APP_SuperUser_EMAIL:"areesh.kumar@p2fsemi.com"// Initial value as an empty string
-    
-    
- }
-export default Backendapi
+  REACT_APP_BACKEND_API_URL: "http://localhost:9011",
+  REACT_APP_SuperUser_EMAIL: "areesh.kumar@p2fsemi.com", // Initial value as an empty string
+};
+export default Backendapi;
 
 //http://13.232.248.126 bhavya@p2fsemi.com vasikaran@p2fsemi.in areesh.kumar@p2fsemi.com
